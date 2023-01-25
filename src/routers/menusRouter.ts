b3e3d28c605b81cfed6caa4b5eee5b140745afb2
Titou <menardjc.33@gmail.com>
2023-01-25 13:43:39 +0100

    feat : menu update and delete

--- src/routers/menusRouter.ts
+++ src/routers/menusRouter.ts
@@ -8,6 +8,6 @@ menusRouter.get('/', menusController.getAllMenus);
 menusRouter.get('/nom/' ,menusController.getMenuByName);
 menusRouter.post('/' , menusController.createMenu);
 menusRouter.put('/:id' ,menusController.updateMenu);
-//menusRouter.delete('/:id' ,menusController.deleteMenu);
+menusRouter.delete('/:id' ,menusController.deleteMenu);
 
 export default menusRouter;
--- src/services/menusServices.ts
+++ src/services/menusServices.ts
@@ -3,61 +3,59 @@ import { Menus } from "../entity/Menus";
 
 const dbManager = AppDataSource.manager;
 
-export class MenusService{
-    async AllMenus(){
+export class MenusService {
+    async AllMenus() {
         const menus: Menus[] | undefined = await Menus.find();
 
-        if (menus[0]){
+        if (menus[0]) {
             return menus;
         }
         return undefined;
     }
-    async menuName(nom : string): Promise <Menus | undefined> {
-        const data = await Menus.findBy({menu_nom: nom});
-        if (data[0]){
+    async menuName(nom: string): Promise<Menus | undefined> {
+        const data = await Menus.findBy({ menu_nom: nom });
+        if (data[0]) {
             return data[0];
         }
         return undefined;
     }
-    async addMenu(nom: string, prix: number){
+    async addMenu(nom: string, prix: number) {
         const newMenu = new Menus();
         newMenu.menu_nom = nom;
-        newMenu.menu_prix= prix;
+        newMenu.menu_prix = prix;
 
         await dbManager.save(newMenu);
         const menuOk = await Menus.findOneBy({
             menu_nom: nom,
         })
 
-        if(menuOk){
+        if (menuOk) {
             return menuOk;
         }
         return undefined;
     }
-    async upMenu(id : number ,nom : string, prix: number){
-        const oldMenu = await Menus.findOneBy({ menu_id : id});
+    async upMenu(id: number, nom: string, prix: number) {
+        const oldMenu = await Menus.findOneBy({ menu_id: id });
 
         oldMenu.menu_nom = nom;
         oldMenu.menu_prix = prix;
 
-        await Menus.save(oldMenu);
-        const menuChanged = await Menus.findOneBy({
-            menu_nom: nom,
-        })
-        if (menuChanged){
+        const menuChanged = await Menus.save(
+            oldMenu
+        )
+        if (menuChanged) {
             return menuChanged;
-        }
+        } 
         return undefined;
     }
-    async suppMenu(menu : Menus): Promise<boolean | undefined>{
-        await Menus.remove(menu);
+    async suppMenu(id: number){
+        const idMenu = await Menus.findOneBy({menu_id : id});
+        idMenu.menu_id = id;
 
-        const idMenu = menu.menu_id
-        const menuOk =await Menus.findBy({
-            menu_id : idMenu
-        })
-        if (menuOk === undefined ) {
-            return true;
+        const menuSupp = await Menus.remove(idMenu);
+
+        if (menuSupp) {
+            return menuSupp;
         }
         return undefined;
 
